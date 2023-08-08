248f8062d448e061cbbd9ae40eff3148dd5b27ee
ragav7332 <96164910+ragav7332@users.noreply.github.com>
2023-08-08 17:37:36 +0530

    Add files via upload

---
+++ src/Header.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+//header
+//cart
+//sign-in
+
+export default function Header(props) {
+  return (
+    <header className="block row center">
+      <div>
+        <a href="#/">
+          <h1>Small Shopping Cart</h1>   
+        </a>
+      </div>
+      <div>
+        <a href="#/cart">
+          Cart{' '}
+          {props.countCartItems ? (
+            <button className="badge">{props.countCartItems}</button>
+          ) : (
+            ''
+          )}
+        </a>{' '}
+        <a href="#/signin"> SignIn</a>
+      </div>
+    </header>
+  );
+}
---
+++ src/data.jsx
@@ -0,0 +1,43 @@
+//list of products presenting in the shop
+
+
+const data = {products:[{
+    id:"1",
+    name :"Head-set",
+    price:1500,
+    image:"https://www.itbestop.com/wp-content/uploads/2020/01/61QPS9fMJZL.jpg",
+},
+{
+    id:"2",
+    name :"Clock",
+    price:1200,
+    image:"https://s.yimg.com/aah/yhst-136665555954272/silent-knight-alarm-clock-by-bulova-10.gif",
+},
+{
+    id:"3",
+    name :"George",
+    price:1000,
+    image:"https://cdn15.wildcraft.com/web-images/large/styles/QQXJDRLIASA/1580099454701/1.JPG",
+},
+{
+    id:"4",
+    name :"Head-set",
+    price:1500,
+    image:"https://tse1.mm.bing.net/th?id=OIP.zFuhX2Ykb9yoH4aPEX4sZwHaHa&pid=Api&P=0&w=300&h=300",
+},
+{
+    id:"5",
+    name :"Car",
+    price:120000000,
+    image:"https://www.lfmmag.com/wp-content/uploads/2018/05/rolls-royce-cullinan-1-1-1170x780.jpg",
+},
+{
+    id:"6",
+    name :"Bat",
+    price:1000,
+    image:"https://assets.ajio.com/medias/sys_master/root/20210719/0MHH/60f5a333aeb269a9e347467b/-473Wx593H-4923366570-multi-MODEL.jpg",
+},
+
+],
+};
+export default data;
